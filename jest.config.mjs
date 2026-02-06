dd425222ea9c5724298a44b922a2245cad48fe0e
Yaolong Zhu <yaolong0612@gmail.com>
2026-02-06 22:12:34 +1100

    create template

---
+++ jest.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["./src/tests"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+};
